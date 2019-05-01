d5183a4d8f32d545f45443df0492e50a3b9643f2
Vũ Tùng Dương <duongvutung1998@gmail.com>
2019-05-01 14:11:49 +0700

    change port to be available of any env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log("Application listen on port: " + port);
