2f9a6e25bd54ad459be29e999f13c10d7d27ee5b
Vũ Tùng Dương <duongvutung1998@gmail.com>
2019-05-26 22:52:02 +0700

    delete ";"

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const methodOverride = require("method-override");
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require("express")
+const bodyParser = require("body-parser")
+const methodOverride = require("method-override")
+const app = express()
+const port = process.env.PORT || 3000
 
-const songs = require("./routes/songs.js");
-const songsbucket = require("./routes/songsbucket");
+const songs = require("./routes/songs.js")
+const songsbucket = require("./routes/songsbucket")
 
 app.listen(port, () => {
-    console.log("Application listen on port: " + port);
+    console.log("Application listen on port: " + port)
 });
 
 
-app.use(methodOverride("_method"));
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.set("views", "./views");
+app.use(methodOverride("_method"))
+app.use(express.static("public"))
+app.set("view engine", "ejs")
+app.set("views", "./views")
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 // app.use("/api/songs", songs);
-app.use("/api/songs", songsbucket);
+app.use("/api/songs", songsbucket)
 
 app.get("/", (req, res) => {
     return res.send("Welcome to MyTune")
     // return res.render("index");
-});
+})
 
 app.get("/test-upload", (req, res) => {
-    res.render("index", { files: false });
+    res.render("index", { files: false })
 })
